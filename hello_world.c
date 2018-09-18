c8ec4591e21227a6c01078fd7648774b615714d0
Wallinger Marc <zadolux@inscomers.net>
2018-09-18 09:43:11 +0200

    Initial hello world code added

---
+++ hello_world.c
@@ -0,0 +1,17 @@
+/*----------------------------------------------------------
+ *				HTBLA-Leonding / Class: 2AHIF
+ * ---------------------------------------------------------
+ * Exercise Number: 1
+ * Title:			Hello World
+ * Author:	  Wallinger Marc
+ * ----------------------------------------------------------
+ * Description:
+ * Simple "Hello World" program
+ * ----------------------------------------------------------
+*/
+#include <stdio.h>
+
+int main(int argc, char const *argv[]) {
+  printf("Hello Marc Wallinger!\n");
+  return 0;
+}
